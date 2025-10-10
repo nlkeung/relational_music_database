6c1b49e8694315580f754e19c3f629d0a7f6c094
nlkeung <nlkeung@yahoo.com>
2025-10-09 23:31:36 -0400

    Removed singles from song-album relationship

--- songs_from_playlist.py
+++ songs_from_playlist.py
@@ -15,11 +15,11 @@ song_album = {}
 song_artist = set()
 song_playlist = {}
 
-DATA_DIR = "temp_data"
+DATA_DIR = "data"
 os.makedirs(f"{DATA_DIR}", exist_ok=True)
 
 
-def load_objects():
+def load_data():
     global playlists, songs, artists_to_check, albums_to_check, song_album, song_artist, song_playlist
     # --------- ENTITIES ---------
     # Playlists - dict
@@ -79,39 +79,6 @@ def load_objects():
 # ------- HELPER FUNCTIONS -------
 
 """
-Saves all necessary attributes for Song entity, along with all of its relationships.
-Relationships include song_artist, song_album, song_playlist
-"""
-def save_song(track):
-    song_id = track["id"]
-    # Attributes
-    if song_id not in songs:
-        songs[song_id] = {
-            "songTitle": track["name"],
-            "duration": track["duration_ms"],
-            "releaseDate": track["album"]["release_date"],
-            "popularity": track.get("popularity", None),
-            "artURL": track["album"]["images"][0]["url"] if track["album"].get("images") else None
-        }
-    # Relationships
-    # Albums
-    album_id = track["album"]["id"]
-    if album_id not in albums_to_check:
-        albums_to_check.add(album_id)
-    if (song_id, album_id) not in song_album:
-        song_album[(song_id, album_id)] = {
-            "trackNumber": track["track_number"]
-        }
-    # Artists
-    for artist in track["artists"]:
-        artist_id = artist["id"]
-        if artist_id not in artists_to_check:
-            artists_to_check.add(artist_id)
-        if (song_id, artist_id) not in song_artist:
-            song_artist.add((song_id, artist_id))
-    # Playlist - handled in main loop
-
-"""
 Grabs the item objects associated with the playlist ID. This function handles
 rate limits from Spotify API and accumulates all entries across pagination
 Input: playlist_id, Spotify client credentials
@@ -143,9 +110,72 @@ def get_playlist_items(playlist_id, sp):
     return all_items
 
 """
+This function takes in track information and looks up its associated album ID.
+It then makes another API call to check if the album is actually a single or a
+compilation. If it is one of the two, it is not an album, and we do not add it to 
+albums_to_check or song_album.
+"""
+def process_track_album(track, sp):
+    song_id = track["id"]
+    album_id = track["album"]["id"]
+
+    # Look up if album is an "album", "single", or "compilation". If not an album, skip
+    while True:
+        try:
+            album = sp.album(album_id)
+            break
+        except spotipy.SpotifyException as e:
+            if e.http_status == 429:
+                retry_after = int(e.headers.get("Retry-After", 1))
+                print(f"❌ Rate limit hit. Retrying after {retry_after} seconds.\n")
+                time.sleep(retry_after)
+            else:
+                raise
+    if album.get("album_type") in ["single", "compilation"]:
+        # Album is actually a single or compilation. Do not include this in the relationship
+        return
+    
+    else:
+        if album_id not in albums_to_check:
+            albums_to_check.add(album_id)
+        if (song_id, album_id) not in song_album:
+            song_album[(song_id, album_id)] = {
+                "trackNumber": track["track_number"]
+            }
+
+"""
+Saves all necessary attributes for Song entity, along with all of its relationships.
+Relationships include song_artist, song_album, song_playlist
+"""
+def save_song(track, sp):
+    song_id = track["id"]
+    # Attributes
+    if song_id not in songs:
+        songs[song_id] = {
+            "songTitle": track["name"],
+            "duration": track["duration_ms"],
+            "releaseDate": track["album"]["release_date"],
+            "popularity": track.get("popularity", None),
+            "artURL": track["album"]["images"][0]["url"] if track["album"].get("images") else None
+        }
+    # Relationships
+    # Albums
+    process_track_album(track, sp)
+    # Artists
+    for artist in track["artists"]:
+        artist_id = artist["id"]
+        if artist_id not in artists_to_check:
+            artists_to_check.add(artist_id)
+        if (song_id, artist_id) not in song_artist:
+            song_artist.add((song_id, artist_id))
+    # Playlist - handled in main loop
+
+"""
 Checkpoint data in case of rate limits or other errors
 """
 def checkpoint():
+    print(f"✅ Checkpointing...")
+
     # Playlists
     with open(f"{DATA_DIR}/playlists.json", "w") as f:
         json.dump(playlists, f)
@@ -176,16 +206,16 @@ def checkpoint():
 
 
 
-# ------- LOAD DATA -------
+# ------- MAIN -------
 def main():
     # Log in to Spotify
     auth_manager = SpotifyClientCredentials()
     sp = spotipy.Spotify(auth_manager=auth_manager)
     
     # Load saved JSONs
-    load_objects()
+    load_data()
 
-    playlist_id = "6qSYIKJihVKpWr2HDeHjxS"
+    playlist_id = "6UeSakyzhiEt4NB3UAd6NQ"
     playlist_info = sp.playlist(playlist_id)
     playlist_name = playlist_info["name"]
 
@@ -210,17 +240,15 @@ def main():
                     "songOrder": song_index
                 } 
             # Song Entity and Other Relationships   
-            save_song(track)
+            save_song(track, sp)
 
             # Checkpointing
             if song_index % 50 == 0:
-                print(f"✅ Checkpointing...")
                 checkpoint()
 
             song_index += 1
     except Exception as e:
         print(f"⚠️ Error occurred: {e}\n")
-        print(f"✅ Checkpointing...")
         checkpoint()
 
     checkpoint()
