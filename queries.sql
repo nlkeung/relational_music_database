a411e32fd8bab54e40072a17e81d6eed1007627d
nlkeung <nlkeung@yahoo.com>
2025-10-13 20:21:07 -0400

    Edited queries

--- queries.sql
+++ queries.sql
@@ -1,26 +1,30 @@
 -- Query 1: Multi-table JOIN
 -- Find all songs by artists that a user follows
-SELECT u.FirstName AS UserName, a.ArtistName, s.SongTitle
+SELECT u.FirstName, u.LastName, u.Username, a.ArtistName, s.SongTitle
 FROM FollowsArtist fa
-JOIN "User" u ON fa.UserID = u.UserID
-JOIN Artist a ON fa.ArtistID = a.ArtistID
+JOIN "Users" u ON fa.UserID = u.UserID
+JOIN Artists a ON fa.ArtistID = a.ArtistID
 JOIN Performs p ON p.ArtistID = a.ArtistID
-JOIN Song s ON p.SongID = s.SongID;
+JOIN Songs s ON p.SongID = s.SongID
+WHERE 
+    u.FirstName = 'Lukas'
+    AND u.LastName = 'Robert'
+    AND s.Popularity >= 90;
 
 -- Query 2: Aggregation
 -- Find the top 5 artists by average song popularity
 SELECT a.ArtistName, ROUND(AVG(s.SongPopularity), 2) AS AvgPopularity
-FROM Artist a
+FROM Artists a
 JOIN Performs p ON a.ArtistID = p.ArtistID
-JOIN Song s ON s.SongID = p.SongID
+JOIN Songs s ON s.SongID = p.SongID
 GROUP BY a.ArtistName
 ORDER BY AvgPopularity DESC
 LIMIT 5;
 
 -- Query 3: Filter + Join
--- Find playlists that contain songs released after 2020
+-- Find playlists that contain songs released before 2020
 SELECT DISTINCT pl.PlaylistName
-FROM Playlist pl
-JOIN InPlaylist ip ON pl.PlaylistID = ip.PlaylistID
-JOIN Song s ON s.SongID = ip.SongID
-WHERE s.SongReleaseDate >= '2020-01-01';
+FROM Playlists pl
+JOIN InPlaylists ip ON pl.PlaylistID = ip.PlaylistID
+JOIN Songs s ON s.SongID = ip.SongID
+WHERE s.SongReleaseDate > '2023-01-01';
